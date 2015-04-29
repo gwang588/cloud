1bb41659974b4d025ba8e3ddac747b44a7874d7a
unknown <gwang46@WITSUSPB713TG.wks.jnj.com>
2015-04-29 16:02:33 -0400

    init

---
+++ src/main/java/cloud/WebApp.java
@@ -0,0 +1,29 @@
+package cloud;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+ 
+public class WebApp extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[0];
+    }
+ 
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{ WebAppConfig.class };
+    }
+ 
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{ "/" };
+    }
+ 
+    @Configuration
+    @EnableWebMvc
+    @ComponentScan("controller")
+    public static class WebAppConfig {
+    }
+}
